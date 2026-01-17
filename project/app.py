dfd870edb602a268043df3b755148e5239bd8ac1
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2026-01-17 16:19:04 -0300

    Recebendo arquivos .txt e .pdf

--- project/app.py
+++ project/app.py
@@ -3,25 +3,54 @@ from flask import request, render_template, Flask
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.get("/")
 def index():
-    result = None
-    response = None
-    text = ""
+    return render_template("index.html")
 
-    if request.method == "POST":
-        text = request.form.get("email_text")
-
-        if text:
-            category = classify_email(text)
-            response = generate_response(text, category)
-            result = category
+@app.post("/")
+def text_email():
+    text = request.form.get("email_text")
+    category = classify_email(text)
+    response = generate_response(text, category)
+    result = category
 
     return render_template(
         "index.html",
         result=result,
         response=response,
         text=text
+    )
+
+@app.post("/upload")
+def file_email():
+    file = request.files.get("email_file")
+
+    if not file or not file.filename.endswith((".pdf",".txt")):
+        return "Por favor, envie apenas arquivos PDF ou TXT.", 400
+
+    if file.filename.endswith(".txt"):
+        email_txt = file.read().decode("utf-8")
+
+        category = classify_email(email_txt)
+        response = generate_response(email_txt, category)
+        return render_template(
+            "index.html",
+            category=category,
+            reply=response
+        )
+
+    email_text = extrair_pdf(file)
+
+    if not email_text:
+        return "Não foi possível extrair texto do PDF.", 400
+
+    category = classify_email(email_text)
+    response = generate_response(email_text, category)
+
+    return render_template(
+        "index.html",
+        category=category,
+        reply=response
     )
 
 if __name__ == "__main__":
--- project/classifier.py
+++ project/classifier.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from openai import OpenAI
+from pypdf import PdfReader
 import os
 
 load_dotenv()
@@ -22,9 +23,11 @@ def classify_email(text):
                 
                          Improdutivo: Emails que não necessitam de uma ação imediata
                           (ex.: mensagens de felicitações, agradecimentos).
+                         
+                         Diversos: Emails promocionais (ex.: Ofertas, promoções)
                           
                           Responda somente com 'Produtivo' ou 'Improdutivo'.
-                          Emails 'promocionais' e 'diversos' categorizar como improdutivo.
+                          Emails 'promocionais' e 'diversos' categorizar como 'Diversos'.
                     """
                 )
             },
@@ -38,8 +41,10 @@ def classify_email(text):
 
     return category.choices[0].message.content.strip()
 
-
 def generate_response(email, category):
+    name = os.getenv('NAME_RESPONSE')
+    job = os.getenv('POSITION_RESPONSE')
+
     response = openAi.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
@@ -47,10 +52,11 @@ def generate_response(email, category):
                 "role" : "system",
                 "content" : (
                     f"""
-                    Você é um assistente que responde emails de acordo com a categoria desse email.
+                    Você é um assistente que responde emails de acordo com a categoria.
                     Após atenciosamente usar os seguintes dados:
-                    [Seu Nome] = Thalles Nascimento
-                    [Seu Cargo] = Tecnologia da Informação
+                    [Seu Nome] = {name}
+                    [Seu Cargo] = {job}
+                    
                     Gere uma resposta clara, objetiva e simpática para o email recebido, considerando
                     a categoria '{category}'. Para categoria 'Produtivo' sua resposta precisa ser apenas automática e que 
                     o email será respondido posteriormente pelo responsável.
@@ -66,3 +72,12 @@ def generate_response(email, category):
     )
 
     return response.choices[0].message.content.strip()
+
+def extrair_pdf(file):
+    pdf = PdfReader(file)
+    text = ""
+
+    for page in pdf.pages:
+        text += page.extract_text() or ""
+
+    return text.strip()
